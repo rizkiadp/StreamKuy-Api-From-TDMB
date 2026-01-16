c2a95f06785ca39143389a4902e9ec8a7c1118fc
Rizki <kyyri@example.com>
2026-01-16 13:59:09 +0700

    vercell

--- server.js
+++ server.js
@@ -97,6 +97,8 @@ app.get('/watch/tv/:id', async (req, res) => {
     });
 });
 
-app.listen(PORT, () => {
-    console.log(`Server running at http://localhost:${PORT}`);
+
+
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Server running on port ${PORT}`);
 });
